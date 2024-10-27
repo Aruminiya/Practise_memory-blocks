ea2dee8d9103fa23f1f5bdd6509bbb015dc7f986
Aruminiya <leo80103@gmail.com>
2024-10-27 16:18:22 +0800

    feat: 部署專案設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/practise-memory-blocks/",
   server: {
     port: 3001,
   },
